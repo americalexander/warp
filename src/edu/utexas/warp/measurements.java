c83cd4702fdaf26810aaf0ea418cbf9c5f39fb17
Hiroshi Ahsan <hiroshi.ahsan@gmail.com>
2018-04-26 21:17:19 -0500

    Create measurements.java

---
+++ src/edu/utexas/warp/measurements.java
@@ -0,0 +1,32 @@
+package edu.utexas.warp;
+
+public class Measurements {
+
+	public static void main (String[] args) {
+		//the method will take in the H and T matrix from other class and returns AEC, relative gap, SPTT, TSTT needed for both convex combination and simplicial decomposition.
+	}
+	
+	public int totalDemand() {
+		//the method returns total demand by summing up all the values of H matrix.
+	}
+	
+	public double SPTT() {
+		//the method returns SPTT by analyzing the matrix provided using iterations.
+	}
+	
+	public double TSTT() {
+		//the method returns TSTT by analyzing the matrix provided using iterations.
+	}
+	
+	public double AEC() {
+		//the method returns AEC by analyzing the matrix provided using iterations.
+	}
+	
+	public double relativeGap() {
+		//the method returns relative gap {1/2, 1/3, 1/4, 1/5, .......} by analyzing the matrix provided using iterations.
+	}
+	
+	public double deltaH() {
+		//the method returns Delta H.
+	}
+}
