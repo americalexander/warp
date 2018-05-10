54bad9a24c8ea133b857e205d0c8734b0782cd4a
William E. Alexander <williamericalexander@gmail.com>
2018-05-09 22:32:59 -0500

    "Done" with measurements - AEC & relative gap now implemented

--- src/edu/utexas/warp/Measurements.java
+++ src/edu/utexas/warp/Measurements.java
@@ -1,26 +1,49 @@
 package edu.utexas.warp;
 
-public class Measurements {
+import java.util.Map;
 
-	public Double totalDemand() {
-		//the method returns total demand by summing up all the values of H matrix.
+public class Measurements {
+	
+	//the method returns total demand by summing up all the values of H matrix.
+	public Double totalDemand(Map<Double, Map<Path, Double>> currentH) {
+		Double demand = Double.valueOf(0);
+		for (Map<Path,Double> te : currentH.values()) {
+			for (Double d : te.values()) {
+				demand += d;
+			}
+		}
+		return demand;
 	}
 	
-	public Double SPTT() {
-		//the method returns SPTT by analyzing the matrix provided using iterations.
+
+	//the method returns TTT by analyzing the matrix provided using iterations.
+	public Double TTT(Map<Double, Map<Path, Double>> hMatrix) {
+		Double totalTravelTime = Double.valueOf(0);
+		for (Double depTime : hMatrix.keySet()) {
+			for (Path p : hMatrix.get(depTime).keySet()) {
+				Double pathTime = Double.valueOf(0);
+				for (Link l : p) {
+					pathTime += l.travelTimeEnter(depTime+pathTime);
+				}
+				totalTravelTime += pathTime * hMatrix.get(depTime).get(p);
+			}
+		}
+		return totalTravelTime;
 	}
 	
-	public Double TSTT() {
-		//the method returns TSTT by analyzing the matrix provided using iterations.
+	//the method returns AEC by analyzing the matrix provided using iterations.
+	public Double AEC(
+			Map<Double,Map<Path,Double>> currentH, 
+			Map<Double,Map<Path,Double>> shortPathH, 
+			Network net) {
+		return (TTT(currentH)-TTT(shortPathH))/totalDemand(currentH);
 	}
 	
-	public Double AEC() {
-		//the method returns AEC by analyzing the matrix provided using iterations.
-		return (TSTT()-SPTT())/totalDemand();
-	}
-	
-	public Double relativeGap() {
-		//the method returns relative gap {1/2, 1/3, 1/4, 1/5, .......} by analyzing the matrix provided using iterations.
-		return (TSTT()-SPTT())/SPTT();
+	//the method returns relative gap {1/2, 1/3, 1/4, 1/5, .......} by analyzing the matrix provided using iterations.
+	public Double relativeGap(
+			Map<Double,Map<Path,Double>> currentH, 
+			Map<Double,Map<Path,Double>> shortPathH, 
+			Network net) {
+		return (TTT(currentH)-TTT(shortPathH))/TTT(shortPathH);
 	}
 }
